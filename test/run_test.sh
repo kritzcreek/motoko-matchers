590a96ce2c0d5494d7381c7898f2680e16b123c1
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2023-03-24 18:15:07 +0100

    Makes Unit tests actual tests, rather than examples
    We now have a matcher that lets us check a test fails with a
    particular error message

--- test/run_test.sh
+++ test/run_test.sh
@@ -7,10 +7,4 @@ echo "Checking Canister.mo compiles"
 $(vessel bin)/moc $(vessel sources) ../src/Canister.mo
 
 $(vessel bin)/moc $(vessel sources) -wasi-system-api Test.mo
-if wasmtime Test.wasm ; then
-    echo "Tests failed to fail"
-    exit 1
-else
-    echo "Tests successfully failed"
-    exit 0
-fi
+wasmtime Test.wasm
